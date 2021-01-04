b357db1497936f469f3e66348ee1d1471f8352eb
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-01-05 01:53:49 +0700

    Try AIML

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const AIMLParser = require('aimlparser')
+
 const app = express()
 const port = process.env.PORT || 4000
+const aimlParser = new AIMLParser({ name:'KPSBot' })
+
+aimlParser.load(['./test-aiml.xml'])
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/webhook', (req, res) => {
-    res.sendStatus(200)
-})
-
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    let msg = req.body.events[0].message.text
+    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
+        reply(reply_token, answer)
+    })
     res.sendStatus(200)
 })
 
 app.listen(port)
 
-function reply(reply_token) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {+QxipMpLXr8Cp3BlsKxhUnreP6G5gh7VFTLDhAhEfuSlKZJUh0zh4W9YYNSs/kECJ6s9lGqepdKWX1IlTzmStX78bOy0Jf4a7aUmS2al2dtcu6qrKL2boOWRAP1+v4TOdN2mmH+v89ElYeacnAwSzwdB04t89/1O/w1cDnyilFU=}'
     }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
-        },
-        {
-            type: 'text',
-            text: 'How are you?'
+            text: msg
         }]
     })
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
