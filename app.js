d723e2be9993a3595501136f21bd95b4f8707a42
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-01-05 15:20:29 +0700

    Fix enviroment config

--- app.js
+++ app.js
@@ -48,75 +48,76 @@ const replyText = (token, texts) => {
 
 // callback function to handle a single event
 function handleEvent(event) {
-  switch (event.type) {
-    case 'message':
-      const message = event.message;
-      switch (message.type) {
-        case 'text':
-          return handleText(message, event.replyToken);
-        case 'image':
-          return handleImage(message, event.replyToken);
-        case 'video':
-          return handleVideo(message, event.replyToken);
-        case 'audio':
-          return handleAudio(message, event.replyToken);
-        case 'location':
-          return handleLocation(message, event.replyToken);
-        case 'sticker':
-          return handleSticker(message, event.replyToken);
+    console.log(event);
+    switch (event.type) {
+        case 'message':
+            const message = event.message;
+            switch (message.type) {
+                case 'text':
+                    return handleText(message, event.replyToken);
+                case 'image':
+                    return handleImage(message, event.replyToken);
+                case 'video':
+                    return handleVideo(message, event.replyToken);
+                case 'audio':
+                    return handleAudio(message, event.replyToken);
+                case 'location':
+                    return handleLocation(message, event.replyToken);
+                case 'sticker':
+                    return handleSticker(message, event.replyToken);
+                default:
+                    throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+            }
+
+        case 'follow':
+            return replyText(event.replyToken, 'Got followed event');
+
+        case 'unfollow':
+            return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+        case 'join':
+            return replyText(event.replyToken, `Joined ${event.source.type}`);
+
+        case 'leave':
+            return console.log(`Left: ${JSON.stringify(event)}`);
+
+        case 'postback':
+            let data = event.postback.data;
+            return replyText(event.replyToken, `Got postback: ${data}`);
+
+        case 'beacon':
+            const dm = `${Buffer.from(event.beacon.dm || '', 'hex').toString('utf8')}`;
+            return replyText(event.replyToken, `${event.beacon.type} beacon hwid : ${event.beacon.hwid} with device message = ${dm}`);
+
         default:
-          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
-      }
-
-    case 'follow':
-      return replyText(event.replyToken, 'Got followed event');
-
-    case 'unfollow':
-      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
-
-    case 'join':
-      return replyText(event.replyToken, `Joined ${event.source.type}`);
-
-    case 'leave':
-      return console.log(`Left: ${JSON.stringify(event)}`);
-
-    case 'postback':
-      let data = event.postback.data;
-      return replyText(event.replyToken, `Got postback: ${data}`);
-
-    case 'beacon':
-      const dm = `${Buffer.from(event.beacon.dm || '', 'hex').toString('utf8')}`;
-      return replyText(event.replyToken, `${event.beacon.type} beacon hwid : ${event.beacon.hwid} with device message = ${dm}`);
-
-    default:
-      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
-  }
+            throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+    } 
 }
 
 function handleText(message, replyToken) {
-  return replyText(replyToken, message.text);
+    return replyText(replyToken, message.text);
 }
 
 function handleImage(message, replyToken) {
-  return replyText(replyToken, 'Got Image');
+    return replyText(replyToken, 'Got Image');
 }
 
 function handleVideo(message, replyToken) {
-  return replyText(replyToken, 'Got Video');
+    return replyText(replyToken, 'Got Video');
 }
 
 function handleAudio(message, replyToken) {
-  return replyText(replyToken, 'Got Audio');
+    return replyText(replyToken, 'Got Audio');
 }
 
 function handleLocation(message, replyToken) {
-  return replyText(replyToken, 'Got Location');
+    return replyText(replyToken, 'Got Location');
 }
 
 function handleSticker(message, replyToken) {
-  return replyText(replyToken, 'Got Sticker');
+    return replyText(replyToken, 'Got Sticker');
 }
 
 app.listen(port, () => {
-  console.log(`listening on ${port}`);
+    console.log(`listening on ${port}`);
 });
